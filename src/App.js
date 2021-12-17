dabe395393669297977eb8a22ab6a7f1263c6293
franckcastle <ouailiwajdi4@gmail.com>
2021-12-17 19:44:19 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import ena from './wajdi-ouaili.jpg';
 import './App.css';
 import ProfileComponent from './components/ProfileComponent';
+
+import React,{ useState } from 'react';
 function App() {
-  const fullname="Wajdi Ouaili"
+  const fullname="Wajdi Ouaili";
+  let a=true
+  const [toggle,setToggle]=useState(a);
 
   const hundlename = () => {
     return alert(`hello ${fullname}`)
-  }
+  };
+  const toggler=()=>{
+    setToggle( (a) => !a);
+  };
   return (
     <div className="App">
-      <h1>Aslema nes lkoool </h1>
+      <div className={toggle ? "active": ""}>
+      <h1 >person's profile</h1>
       <ProfileComponent handle={hundlename} fullName={fullname} bio='salamoualaykom' profession='etudiant'>
           {ena}
         </ProfileComponent>
+        </div>
+        <button onClick={toggler}>toggle</button>
     </div>
   );
 }
